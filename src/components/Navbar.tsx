ea5148f0744325cd938687c24b995ab36f155aea
Gal Mizrahi <67601537+galm85@users.noreply.github.com>
2023-03-16 09:14:59 +0200

    create sidenav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { TbPigMoney } from 'react-icons/tb';
 
 const Navbar = () => {
+
+  const [displaySidenav,setDisplaySidenav] = useState<Boolean>(false);
+
+  
+
   return (
     <header className="header">
         <div className="header__logo-container">
-            <img src="" alt="" className="logo" />
-            logo
+           <TbPigMoney size="3rem"/>
         </div>
         <div className="header__menu">
-            menu
+            <GiHamburgerMenu onClick={()=>setDisplaySidenav(!displaySidenav)}/>
+            
         </div>
+        <div className={displaySidenav?'header__sidenav header__sidenav--open':'header__sidenav header__sidenav--close'}>
+            <ul>
+              <li>History</li>
+              <li>Paycheck</li>
+              <li>History</li>
+              <li>History</li>
+            </ul>
+        </div>
+       
     </header>
   )
 }
