3c596558a7a4733509c7f0da828d2523e1382fdb
Gal <galg@elad.co.il>
2023-06-04 14:06:59 +0300

    create homepage and sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {useEffect,useState} from 'react';
-import { Expenses,Balance,Navbar} from "./components";
-import { Expense } from './types/Expense';
-import axios from 'axios';
+import { Navbar} from "./components";
+import { Routes,Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Home from "./pages/Home";
 
 
 //functions
@@ -11,56 +11,18 @@ import axios from 'axios';
 
 function App() {
   
-    const [fixedExpenses, setFixedExpenses] = useState<Expense[]>([]);
-    const [occasionalExpenses, setOccasionalExpenses] = useState<Expense[]>([]);
-    const [paycheck, setPaycheck] = useState<number>(10000);
-    const [outcome,setOutcome] = useState<number>(0)
-
- 
-
-    useEffect(()=>{
-       
-      const getData = async()=>{
-
-        let fixed:Expense[] = [];
-        let occasional:Expense[] = [];
-        let totalFixed:number = 0;
-        let totalOccasional:number = 0;
-      
-        const {data} = await axios.get(`${process.env.REACT_APP_SERVER_API}/expences/6471e6843eedbda246a967eb`);
-       
-        data.forEach((expence:Expense)=>{
-          if(expence.type === 'fixed'){
-            fixed.push(expence);
-            totalFixed += expence.sum;
-          }else{
-            occasional.push(expence);
-            totalOccasional += expence.sum;
-          }
-        });
-
-
-        setFixedExpenses(fixed);
-        setOccasionalExpenses(occasional);
-        setOutcome(totalFixed + totalOccasional);
-      
-      
-      
-      }
-
-      getData();
-
-    },[])
-
- 
-
 
 
   return (
     <div className="App">
        <Navbar/>
-       <Balance paycheck={paycheck} outcome={outcome}/>
-       <Expenses  occasionalExpenses={occasionalExpenses} fixedExpenses={fixedExpenses}/>
+       <main>
+        <Routes>
+          <Route path='/signin' element={<Signin/>} />
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+       
+       </main>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,62 @@
+import {useEffect,useState} from 'react';
+import { Expenses,Balance,Navbar} from "../components";
+import { Expense } from '../types/Expense';
+import axios from 'axios';
+import { Routes,Route } from 'react-router-dom';
+
+
+
+//functions
+const Home = () => {
+
+
+    const [fixedExpenses, setFixedExpenses] = useState<Expense[]>([]);
+    const [occasionalExpenses, setOccasionalExpenses] = useState<Expense[]>([]);
+    const [paycheck, setPaycheck] = useState<number>(10000);
+    const [outcome,setOutcome] = useState<number>(0)
+
+
+    useEffect(()=>{
+       
+        const getData = async()=>{
+  
+          let fixed:Expense[] = [];
+          let occasional:Expense[] = [];
+          let totalFixed:number = 0;
+          let totalOccasional:number = 0;
+        
+          const {data} = await axios.get(`${process.env.REACT_APP_SERVER_API}/expences/6471e6843eedbda246a967eb`);
+         
+          data.forEach((expence:Expense)=>{
+            if(expence.type === 'fixed'){
+              fixed.push(expence);
+              totalFixed += expence.sum;
+            }else{
+              occasional.push(expence);
+              totalOccasional += expence.sum;
+            }
+          });
+  
+  
+          setFixedExpenses(fixed);
+          setOccasionalExpenses(occasional);
+          setOutcome(totalFixed + totalOccasional);
+        
+        
+        
+        }
+  
+        getData();
+  
+      },[])
+  
+   
+  return (
+    <div>
+        <Balance paycheck={paycheck} outcome={outcome}/>
+       <Expenses  occasionalExpenses={occasionalExpenses} fixedExpenses={fixedExpenses}/>
+    </div>
+  )
+}
+
+export default Home
